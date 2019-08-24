7085657121a23e7b3c0242b4f739c7b4a545e105
Duc Pham <ducp@voltn.com>
2019-08-24 13:49:42 +0700

    Update cleaner

--- src/cleaner/cleaner.py
+++ src/cleaner/cleaner.py
@@ -4,6 +4,7 @@
 """
 import os
 from collections import namedtuple
+import time
 
 FileObj = namedtuple('FileObj', ['file_path', 'size', 'creation_time'])
 
@@ -16,16 +17,23 @@ class Icleaner(object):
     def _scan_storage(self):
         total_size = 0
         files = []
-        for dirpath, dirnames, filenames in os.walk(self.storage_path):
+        for dirpath, dirnames, filenames in os.walk(self.path):
             for f in filenames:
+                if not f.endswith('.ts'):
+                    continue
+
                 fp = os.path.join(dirpath, f)
+
                 if not os.path.islink(fp):
                     file_size = os.path.getsize(fp)
-                    creation_time = os.path.getmtime()
+                    creation_time = os.path.getmtime(fp)
                     total_size += file_size
                     files.append(FileObj(fp, file_size, creation_time))
 
-        return total_size, files.sort(key= lambda x: x.creation_time)
+        files.sort(key= lambda x: x.creation_time)
+        print("Info: total size {0}, number of files {1}".format(total_size, len(files)))
+
+        return total_size, files
 
     def _get_old_files(self, files_list, current_size, limit_size):
         for f in files_list:
@@ -37,17 +45,17 @@ class Icleaner(object):
 
     def cleaning(self):
         current_size, files_list = self._scan_storage()
-        for f in self._get_old_files(files_list, current_size, current_size - 40 * 1024 * 1024):
+        for f in self._get_old_files(files_list, current_size, self.limit):
             try:
                 print("Info: removing {}".format(f.file_path))
-                #os.remove(f.file_path)
+                os.remove(f.file_path)
             except OSError:
                 print("Error: {} doesn't exist".format(f.file_path))
 
 if __name__ == '__main__':
-    storage_path = '/media/usb'
+    storage_path = '/home/pi/Monitor/NhaDuc'
     limit_size = 5000000000
     cleaner = Icleaner(storage_path, limit_size)
     while True:
         cleaner.cleaning()
-        time.sleep(5)
+        time.sleep(60)
