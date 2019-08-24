100f59f120b54fbd78edb14240482e2425e79f7d
Duc Pham <ducp@voltn.com>
2019-08-24 17:43:12 +0700

    Implement Digest authentication

--- src/python/rtsp_client.py
+++ src/python/rtsp_client.py
@@ -1,9 +1,14 @@
 import socket
 import argparse
+from collections import namedtuple
+import hashlib
+import os
 
 
 RTSP_FIRST_LINE = "{command} rtsp://{host}:{port}/{path} RTSP/1.0\r\n"
 RTSP_HEADER = "{}: {}\r\n"
+
+Response = namedtuple("Response", ["code", "message", "headers"])
 
 
 def parse_input():
@@ -27,12 +32,22 @@ class RTSPClient(object):
     def connect(self):
         self.control_sock.connect((self.server_ip, self.server_port))
 
+    def _parse_response(self, data):
+        """Parse response without SDP"""
+        lines = data.decode("utf-8").split("\r\n\r\n")[0].split("\r\n")
+        first_line = lines[0]
+        protocol, code, message = first_line.split(maxsplit=2)
+        assert protocol == "RTSP/1.0", "Unknown protocol %s" % protocol
+        headers = dict(line.split(": ", maxsplit=1) for line in lines[1:])
+        return Response(int(code), message, headers)
+
     def _send(self, data):
         print("Sending %s" % data)
         self.control_sock.send(data)
-        data = self.control_sock.recv(1024)
+        response = self.control_sock.recv(1024)
         self.cseq += 1
-        return data
+        print("Received %s" % response)
+        return response
 
     def _generate_first_line(self, command):
         return RTSP_FIRST_LINE.format(
@@ -47,15 +62,45 @@ class RTSPClient(object):
         msg += RTSP_HEADER.format("CSeq", self.cseq)
         msg += RTSP_HEADER.format("User-Agent", "Isee v1.0")
         msg += '\r\n'
-        return self._send(bytearray(msg, 'utf-8'))
+        response = self._send(bytearray(msg, 'utf-8'))
+        return self._parse_response(response)
 
-    def describe_request(self):
+    def describe_request(self, authen=None):
         msg = self._generate_first_line("DESCRIBE")
         msg += RTSP_HEADER.format("CSeq", self.cseq)
+        if authen:
+            msg += RTSP_HEADER.format("Authorization", authen)
         msg += RTSP_HEADER.format("User-Agent", "Isee v1.0")
         msg += RTSP_HEADER.format("Accept", "application/sdp")
         msg += '\r\n'
-        return self._send(bytearray(msg, 'utf-8'))
+        response = self._send(bytearray(msg, 'utf-8'))
+        return self._parse_response(response)
+
+    def generate_auth_string(
+        self, username, password, realm, method, uri, nonce
+    ):
+        m1_str = username + ":" + realm + ":" + password
+        m1 = hashlib.md5(m1_str.encode("utf-8")).hexdigest()
+        m2_str = method + ":" + uri
+        m2 = hashlib.md5(m2_str.encode('utf-8')).hexdigest()
+        response_str = m1 + ":" + nonce + ":" + m2
+        response = hashlib.md5(response_str.encode('utf-8')).hexdigest()
+        authen_str = 'Digest '
+        authen_str += 'username="' + username + '", '
+        authen_str += 'realm="' + realm + '", '
+        authen_str += 'algorithm="MD5", '
+        authen_str += 'nonce="' + nonce + '", '
+        authen_str += 'uri="' + uri + '", '
+        authen_str += 'response="' + response + '"'
+        return authen_str
+
+    def authenticate(self, response, username, password, method):
+        uri = self._generate_first_line(method).split()[1]
+        digest = response.headers['WWW-Authenticate'].split('"')
+        assert "Digest" in digest[0]
+        realm, nonce = digest[1], digest[3]
+        return self.generate_auth_string(
+            username, password, realm, method, uri, nonce)
 
 
 if __name__ == "__main__":
@@ -63,6 +108,11 @@ if __name__ == "__main__":
     client = RTSPClient(args.server_ip, args.control_port, args.path)
     client.connect()
     response = client.option_request()
-    print(response)
     response = client.describe_request()
-    print(response)
+    if response.code == 401:
+        print("Server requires authentication")
+        password = os.environ['MYPW']
+        authen_str = client.authenticate(
+            response, "admin", password, "DESCRIBE")
+        print("Generate authentication : %s" % authen_str)
+        response = client.describe_request(authen_str)
